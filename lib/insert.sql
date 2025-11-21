ec168651043d267d706ce0d1cfdb4762135e1246
bran259 <kiprop686@gmail.com>
2025-11-21 16:42:33 +0300

    practce sql tests

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES ('Mr. Chocolate', 'M', 20, 'Brown', 'Calm', 1);
+
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES ('Rowdy', 'M', 15, 'Black', 'Aggressive', 1);
+
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES ('Tabitha', 'F', 10, 'White', 'Shy', 1);
+
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES ('Sergeant Brown', 'M', 25, 'Brown', 'Bold', 1);
+
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES ('Melissa', 'F', 8, 'Black', 'Sweet', 1);
+
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES ('Grinch', 'M', 40, 'Green', 'Grumpy', 0);
+
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES ('Wendy', 'F', 6, 'Grey', 'Friendly', 1);
+
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES (NULL, 'F', 7, 'Brown', 'Mysterious', 0);
